81081b504e4c835ff7ecfb121cf4ce92d17e6729
danyonsatterlee <danyon.satterlee@gmail.com>
2016-09-26 13:06:15 -0700

    background in place. words in correct divs but not actually scattered.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 $( function() {
 
-let screenHeight = window.innerHeight
-let screenWidth = window.innerWidth
+let screenHeight = document.getElementById("insults").innerHeight
+let screenWidth  = document.getElementById("insults").innerWidth
+
 
   let words = `never knew  young body with  old head Thy wit very bitter sweeting it most sharp sauce There no more faith thee than stewed prune I will most humbly take my leave of you you cannot sir take from me anything that will more willingly part withal civility thou seem'st much brain Thou cullionly reeling-ripe vassal Thy bones marrowless thy blood ragged Wart artless full-gorged puttock ruttish fat-kidneyed horn-beast froward dizzy-eyed clotpole errant fool-born hedge-pig puny base-court miscreant one all luxury ass utter foul speeches and to detract rump-fed ronyon spleeny rump-fed lewdster wert best set thy lower part where thy nose stands madman cold ear wax empty  shallow cowardly hind you lie tedious fool churlish rough-hewn blind-worm Thou impertinent ill-breeding giglet Thou art some fool venomed malmsey-nosed whey-face loath to beat thee purpose`.split(' ');
 
 
   for (let i=0; i <words.length; i++){
 
-  let randX =  Math.floor(Math.random() * (screenWidth + 1));
-  let randY = Math.floor(Math.random() * (screenHeight + 1));
+  let randX = Math.floor(Math.random() * (screenWidth + 10));
+  let randY = Math.floor(Math.random() * (screenHeight + 10));
 
     let currentWord=words[i];
 
-    $(`.wrapper`).append(`
+    $(`#insults`).append(`
       <span class="draggable" style="left:${randX}px; top: ${randY}px;">
       ${currentWord}
       </span>
