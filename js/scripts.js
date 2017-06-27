cbcdfae47ed8ab0f9462fd9ddc9fca5d2e24b179
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-27 14:25:30 -0600

    ecma 6 mocha/chai check and problem solving

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 $( function() {
 
-let screenHeight = document.getElementById("insults").innerHeight
-let screenWidth  = document.getElementById("insults").innerWidth
+var screenHeight = window.innerHeight;
+var screenWidth  = window.innerWidth;
 
 
-  let words = `never knew  young body with  old head Thy wit very bitter sweeting it most sharp sauce There no more faith thee than stewed prune I will most humbly take my leave of you you cannot sir take from me anything that will more willingly part withal civility thou seem'st much brain Thou cullionly reeling-ripe vassal Thy bones marrowless thy blood ragged Wart artless full-gorged puttock ruttish fat-kidneyed horn-beast froward dizzy-eyed clotpole errant fool-born hedge-pig puny base-court miscreant one all luxury ass utter foul speeches and to detract rump-fed ronyon spleeny rump-fed lewdster wert best set thy lower part where thy nose stands madman cold ear wax empty  shallow cowardly hind you lie tedious fool churlish rough-hewn blind-worm Thou impertinent ill-breeding giglet Thou art some fool venomed malmsey-nosed whey-face loath to beat thee purpose`.split(' ');
+  var words = `young body old head wit bitter sweeting sharp sauce stewed prune humbly leave anything willingly part withal civility brain  cullionly Wart artless full-gorged puttock ruttish froward dizzy-eyed hedge-pig puny base-court miscreant one luxury ass utter foul speeches detract rump-fed ronyon spleeny rump-fed lewdster wert best set  lower part where  nose stands madman cold ear wax rough-hewn blind-worm impertinent ill-breeding gigvar venomed malmsey-nosed whey-face beat purpose I I thou thou thou was was shall shall`.split(' ');
 
 
-  for (let i=0; i <words.length; i++){
+  for (var i=0; i < words.length; i++){
 
-  let randX = Math.floor(Math.random() * (screenWidth + 10));
-  let randY = Math.floor(Math.random() * (screenHeight + 10));
+    var randX = Math.floor(Math.random() * (screenWidth + 1));
+    var randY = Math.floor(Math.random() * (screenHeight + 1));
 
-    let currentWord=words[i];
+    var currentWord=words[i];
 
-    $(`#insults`).append(`
-      <span class="draggable" style="left:${randX}px; top: ${randY}px;">
+
+
+    $(`body`).append(`
+      <span class="draggable" style = "right:${randX}px; top: ${randY}px;">
       ${currentWord}
       </span>
       `)
@@ -26,41 +28,40 @@ let screenWidth  = document.getElementById("insults").innerWidth
 
 
 
-
     $( ".draggable" ).draggable();
 
   } );
 
+  //
+  // $(function() {
+  //
+  //   var helperHeight = document.getElementById("helperverbs").innerHeight;
+  //   var helperWidth = document.getElementById("helperverbs").innerWidth;
+  //
+  // var helpingVerbs = `are are is is was was were were be be being being been been have  have has has had had having having do does did doing done could should would can shall shall shall shall will will will may may may might might might must must must didn't didn't didn't don't don't don't doesn't a a a a a a a as as as as I I I I I thou thou thou the the the the am am am am am I I I I I I I you you you you you you`.split(' ');
+  //
+  //
+  //
+  //   for (var i=0; i <helpingVerbs.length; i++){
+  //
+  //   var randX =  Math.floor(Math.random() * (helperWidth + 1));
+  //   var randY = Math.floor(Math.random() * (helperHeight + 1));
+  //
+  //     var smallWord=helpingVerbs[i];
+  //
+  //     $(`#helperverbs`).append(`
+  //       <span class="draggable" style="right:${randX}px; top: ${randY}px;">
+  //       ${smallWord}
+  //       </span>
+  //       `)
+  //     }
 
-  $( function() {
-
-    let helperHeight = document.getElementById("helperverbs").innerHeight
-    let helperWidth = document.getElementById("helperverbs").innerWidth
-
-  let helpingVerbs = `are are is is was was were were be be being being been been have  have has has had had having having do does did doing done could should would can shall shall shall shall will will will may may may might might might must must must didn't didn't didn't don't don't don't doesn't a a a a a a a as as as as I I I I I thou thou thou the the the the am am am am am I I I I I I I you you you you you you`.split(' ');
-
-
-
-    for (let i=0; i <helpingVerbs.length; i++){
-
-    let randX =  Math.floor(Math.random() * (helperWidth + 1));
-    let randY = Math.floor(Math.random() * (helperHeight + 1));
-
-      let smallWord=helpingVerbs[i];
-
-      $(`#helperverbs`).append(`
-        <span class="draggable" style="left:${randX}px; top: ${randY}px;">
-        ${smallWord}
-        </span>
-        `)
-      }
-
-
-
-
-
-
-
-      $( ".draggable" ).draggable();
-
-    } );
+    //
+    //
+    //
+    //
+    //
+    //
+    //   $( ".draggable" ).draggable();
+    //
+    // } );
